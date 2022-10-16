f97720467dcaba99b178f865a68c449f96e906d5
joon0000 <junekawin@icloud.com>
2022-10-16 17:01:49 +0700

    add data

--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -27,4 +27,92 @@ func SetupDatabase() {
 	)
 
 	db = database
+	//add example data
+	//emp
+	sirin := Employee{
+		NAME:     "Sirinya",
+		PASSWORD: "zaq1@wsX",
+	}
+	db.Model(&Employee{}).Create(&sirin)
+
+	atta := Employee{
+		NAME:     "Attawit",
+		PASSWORD: "zxvseta",
+	}
+	db.Model(&Employee{}).Create(&atta)
+
+	//Role
+	student := Role{
+		NAME:        "Student",
+		BORROW_DAY:  3,
+		BOOKROOM_HR: 3,
+		BOOKCOM_HR:  4,
+	}
+	db.Model(&Role{}).Create(&student)
+
+	teacher := Role{
+		NAME:        "Teacher",
+		BORROW_DAY:  7,
+		BOOKROOM_HR: 12,
+		BOOKCOM_HR:  12,
+	}
+	db.Model(&Role{}).Create(&teacher)
+
+	//province
+	korat := Province{
+		NAME: "Nakhon Ratchasima",
+	}
+	db.Model(&Province{}).Create(&korat)
+
+	chon := Province{
+		NAME: "Chonburi",
+	}
+	db.Model(&Province{}).Create(&chon)
+
+	bangkok := Province{
+		NAME: "Bangkok",
+	}
+	db.Model(&Province{}).Create(&bangkok)
+
+	//member
+	classic := MemberClass{
+		NAME:     "classic",
+		DISCOUNT: 0,
+	}
+	db.Model(&MemberClass{}).Create(&classic)
+
+	silver := MemberClass{
+		NAME:     "silver",
+		DISCOUNT: 5,
+	}
+	db.Model(&MemberClass{}).Create(&silver)
+
+	gold := MemberClass{
+		NAME:     "gold",
+		DISCOUNT: 10,
+	}
+	db.Model(&MemberClass{}).Create(&gold)
+
+	plat := MemberClass{
+		NAME:     "platinum",
+		DISCOUNT: 20,
+	}
+	db.Model(&MemberClass{}).Create(&plat)
+
+	//user
+	db.Model(&User{}).Create(&User{
+		PIN:       "B6111111",
+		FirstName: "preecha",
+		LastName:  "anpa",
+		CIV:       "1111111111111",
+		PHONE:     "0811111111",
+		EMAIL:     "preechapat@mail.com",
+		PASSWORD:  "1111111111111",
+		ADDRESS:   "ถนน a อำเภอ v",
+		//FK
+		Employee:    sirin,
+		Role:        student,
+		Province:    korat,
+		MemberClass: classic,
+	})
 }
